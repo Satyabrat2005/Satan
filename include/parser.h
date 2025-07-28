f405621ca81d6e9d6d2e63f5b72f6e60af125be1
Satyabrat Sahu <satyabratsahu71@gmail.com>
2025-07-28 20:36:30 +0530

    update parser.h

--- include/parser.h
+++ include/parser.h
@@ -1,15 +1,16 @@
+
 #ifndef PARSER_H
 #define PARSER_H
 
 #include "lexer.h"
 #include <memory>
-
-// ---- AST Node Types ----
+#include <vector>
 
 class Expr {
 public:
     virtual ~Expr() = default;
     virtual void print() const = 0;
+    virtual double evaluate() const = 0;
 };
 
 class LiteralExpr : public Expr {
@@ -17,6 +18,15 @@ public:
     Token value;
     explicit LiteralExpr(Token val) : value(std::move(val)) {}
     void print() const override;
+    double evaluate() const override;
+};
+
+class VariableExpr : public Expr {
+public:
+    Token name;
+    explicit VariableExpr(Token n) : name(std::move(n)) {}
+    void print() const override;
+    double evaluate() const override;
 };
 
 class BinaryExpr : public Expr {
@@ -29,16 +39,49 @@ public:
         : left(std::move(l)), op(std::move(o)), right(std::move(r)) {}
 
     void print() const override;
+    double evaluate() const override;
+};
+
+class Stmt {
+public:
+    virtual ~Stmt() = default;
+    virtual void execute() const = 0;
+};
+
+class VarDecl : public Stmt {
+public:
+    Token name;
+    std::unique_ptr<Expr> initializer;
+
+    VarDecl(Token n, std::unique_ptr<Expr> init)
+        : name(std::move(n)), initializer(std::move(init)) {}
+
+    void execute() const override;
+};
+
+class PrintStmt : public Stmt {
+public:
+    std::unique_ptr<Expr> expr;
+
+    explicit PrintStmt(std::unique_ptr<Expr> e)
+        : expr(std::move(e)) {}
+
+    void execute() const override;
 };
 
 class Parser {
 public:
     explicit Parser(const std::vector<Token>& tokens);
-    std::unique_ptr<Expr> parse();
+    std::vector<std::unique_ptr<Stmt>> parse();
 
 private:
     const std::vector<Token>& tokens;
     int current;
+
+    std::unique_ptr<Stmt> declaration();
+    std::unique_ptr<Stmt> varDeclaration();
+    std::unique_ptr<Stmt> statement();
+    std::unique_ptr<Stmt> printStatement();
 
     std::unique_ptr<Expr> expression();
     std::unique_ptr<Expr> term();
@@ -49,6 +92,7 @@ private:
     bool match(std::initializer_list<TokenType> types);
     bool check(TokenType type) const;
     Token peek() const;
+    Token consume(TokenType type, const std::string& message);
     bool isAtEnd() const;
 };
 
