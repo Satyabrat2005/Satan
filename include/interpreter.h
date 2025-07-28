03ce846d4db9328efe8003f3d424050b2fc06c3f
Satyabrat Sahu <satyabratsahu71@gmail.com>
2025-07-28 20:34:30 +0530

    interpreter.h added compilation

---
+++ include/interpreter.h
@@ -0,0 +1,13 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "parser.h"
+
+class Interpreter {
+public:
+    void interpret(const Expr* expr) const;
+private:
+    void evaluate(const Expr* expr) const;
+};
+
+#endif // INTERPRETER_H
