64e0d09e03e8fd59800f5ad876f2a3b4bc69c811
Satyabrat Sahu <satyabratsahu71@gmail.com>
2025-07-29 22:50:39 +0530

    test_interpreter.cpp updated

--- tests/test_interpreter.cpp
+++ tests/test_interpreter.cpp
@@ -1,17 +1,22 @@
 #include "../include/lexer.h"
 #include "../include/parser.h"
+#include "../include/environment.h"
 #include <iostream>
+
+Environment globalEnv;
 
 int main() {
     std::string source = "let x = 5 + 3 * 2; print x;";
+
     Lexer lexer(source);
     std::vector<Token> tokens = lexer.scanTokens();
 
     Parser parser(tokens);
     std::vector<std::unique_ptr<Stmt>> statements = parser.parse();
 
+    // Use global environment
     for (const auto& stmt : statements) {
-        stmt->execute();
+        stmt->execute(globalEnv);
     }
 
     return 0;
