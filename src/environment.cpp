016f37b246dcac82b0dc682d022b2bdb6f5a1be7
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-08-29 23:27:45 +0530

    Update environment.cpp

--- src/environment.cpp
+++ src/environment.cpp
@@ -2,13 +2,11 @@
 #include <stdexcept>
 #include <iostream>
 
-// Define or overwrite a variable in the environment
 void Environment::define(const std::string& name, double value) {
     values[name] = value;
     std::cout << "[env] " << name << " defined as " << value << std::endl;
 }
 
-// Retrieve a variable's value, or throw a runtime error if undefined
 double Environment::get(const std::string& name) const {
     auto it = values.find(name);
     if (it != values.end()) {
