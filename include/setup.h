7ac88ccfa8a31db514369bf7ddd03c3c14dcdf09
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-05-01 09:27:45 +0530

    Create setup.h

---
+++ include/setup.h
@@ -0,0 +1,15 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#include <string>
+
+// Check all dependencies and print status
+void checkDependencies();
+
+// Auto-install Python ML dependencies
+void installMLDependencies();
+
+// Print Satan version and module info
+void printVersion();
+
+#endif
