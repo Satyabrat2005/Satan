69d207f9ed3f5b2d393ea9b7fc01244757dc04ad
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-06-27 22:14:28 +0530

    Update catch.hpp

--- third_party/catch.hpp
+++ third_party/catch.hpp
@@ -2787,3 +2787,34 @@ namespace Catch {
 // start catch_totals.h
 
 #include <cstddef>
+
+namespace Catch {
+
+    struct Counts {
+        Counts operator - ( Counts const& other ) const;
+        Counts& operator += ( Counts const& other );
+
+        std::size_t total() const;
+        bool allPassed() const;
+        bool allOk() const;
+
+        std::size_t passed = 0;
+        std::size_t failed = 0;
+        std::size_t failedButOk = 0;
+    };
+
+    struct Totals {
+
+        Totals operator - ( Totals const& other ) const;
+        Totals& operator += ( Totals const& other );
+
+        Totals delta( Totals const& prevTotals ) const;
+
+        int error = 0;
+        Counts assertions;
+        Counts testCases;
+    };
+}
+
+// end catch_totals.h
+#include <string>
